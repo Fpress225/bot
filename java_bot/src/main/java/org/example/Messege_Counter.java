d9b53bb327273d2561d10654e25ac03e78602457
botss <sidorovkirill226@gmail.com>
2024-06-24 17:58:10 +0300

    set

---
+++ java_bot/src/main/java/org/example/Messege_Counter.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class Messege_Counter extends TelegramLongPollingBot {
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        switch (update.getMessage().getText().toLowerCase()) {
+            case "пинг":
+                SendMessage sendMessage = new SendMessage();
+                sendMessage.setText("понг");
+                sendMessage.setChatId(update.getMessage().getChatId());
+                try {
+                    execute(sendMessage);
+                } catch (TelegramApiException e) {
+                    throw new RuntimeException(e);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "test5278652_bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "7287070944:AAGGbz-i3jNJslZJTtCIVOcTIFUNxshyO0U";
+    }
+
+}
